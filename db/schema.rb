1dbc3e8d989d4426f2f23b319cc6fb781234734e
Maryam Fattah <mary.fatah@gmail.com>
2023-02-21 15:21:45 +0100

    done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_02_21_125554) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.integer "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
